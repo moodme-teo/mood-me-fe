4a48c2853173d7d372c60bd2b25ab04d05a3b034
dev0leee <devv0leee@gmail.com>
2026-07-05 23:12:47 +0900

    feat : 테스트 텍스트 페이지 추가
    Refs #1

---
+++ src/app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="text-2xl font-semibold">테스트</p>
+    </div>
+  );
+}
